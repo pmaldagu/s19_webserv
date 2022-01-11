530aaa235accf6121fd1f0cd8177768ed52a3c00
Garreth Verhelpen <gverhelp@student.s19.be>
2022-01-11 14:29:33 +0100

    server and client

---
+++ include/webserv.h
@@ -0,0 +1,14 @@
+#ifndef WEBSERV_H
+# define WEBSERV_H
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define PORT 8000
+
+#endif
